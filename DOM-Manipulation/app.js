19534df9d5476c983f3fd1f90d076fa0b5fc816b
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-01-12 23:50:03 +0200

    DOM Manipulation : Traversing the DOM, Creating elements

--- DOM-Manipulation/app.js
+++ DOM-Manipulation/app.js
@@ -24,3 +24,39 @@
 //     item.style.color = 'red'
 // });
 // console.log(items);
+
+// 4 -- Traversing the DOM
+// let val; 
+// const list = document.querySelector('ul.collection');
+// const listItem = document.querySelector('li.collection-item');
+
+//val = listItem;
+//Get child nodes
+// val = list.childNodes;
+// val = list.children;
+// val = list.firstElementChild;
+
+//Get parent nodes
+//val = listItem.parentNode;
+
+//Get siblings 
+// val = listItem.nextSibling;
+// val = listItem.nextElementSibling;
+
+// console.log(val);
+
+// 5 -- Creating elements
+// const li = document.createElement('li');
+// li.className = 'collection-item';
+// li.setAttribute('title','new title');
+// li.appendChild(document.createTextNode('Hello'));
+
+// const link = document.createElement('a');
+// link.className = 'delete-item secondary-content';
+// link.innerHTML = '<i class="fa fa-remove"></i>';
+
+// li.appendChild(link);
+
+// document.querySelector('ul.collection').appendChild(li);
+
+// console.log(li);
