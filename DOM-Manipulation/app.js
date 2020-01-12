7e9a02e836c1cc72ed4a41e31413f1c8cafcbdc8
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-01-12 22:52:52 +0200

    DOM Manipulation : Selectors for single and mupltiple elements

--- DOM-Manipulation/app.js
+++ DOM-Manipulation/app.js
@@ -1,7 +1,26 @@
 // 1-- Document Object examination
-let val; 
-val = document.forms;
-val = document.links;
-val = document.scripts;
+// let val; 
+// val = document.forms;
+// val = document.links;
+// val = document.scripts;
 
-console.log(val);
+// console.log(val);
+
+// 2 -- DOM selectors for single elements
+// console.log(document.getElementById('task-title').id);
+// const taskTitle = document.getElementById('task-title');
+// taskTitle.style.background = 'red';
+// taskTitle.innerText = 'My tasks';
+
+// document.querySelector('li').style.color = 'blue';
+// document.querySelector('li:nth-child(3)').style.color = 'red';
+
+// 3 -- DOM selectors for multiple elements
+// const items = document.getElementsByClassName('collection-item');
+// items[0].style.color = 'red';
+
+// const items = document.querySelectorAll('li');
+// items.forEach(function(item){
+//     item.style.color = 'red'
+// });
+// console.log(items);
