5e9bd932994b92630d08b8acaebb6cbf80ebf8e5
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-01-14 23:05:58 +0200

    Project : Loan Calculator Part 2 - Loan calculations and displaying them to user

---
+++ Project_2-Loan_Calculator/app.js
@@ -0,0 +1,32 @@
+document.getElementById('loan-form').addEventListener('submit', calculateResults);
+
+function calculateResults(e){
+    const amount = document.getElementById('amount');
+    const interest = document.getElementById('interest');
+    const years = document.getElementById('years');
+    const monthlyPayment = document.getElementById('monthly-payment');
+    const totalPayment = document.getElementById('total-payment');
+    const totalInterest = document.getElementById('total-interest');
+    const alertMessage = document.getElementById('alert-message');
+
+    const principal = parseFloat(amount.value);
+    const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+    const calculatedPayments = parseFloat(years.value) * 12;
+
+    //Monthly payments
+    const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+    const monthly = (principal*x*calculatedInterest) / (x - 1);
+
+    if (isFinite(monthly)){
+        monthlyPayment.value = monthly.toFixed(2);
+        totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+        totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+    } else {
+        alertMessage.style.display = 'block';
+        setTimeout(() => {
+            alertMessage.style.display = 'none';
+        }, 3000);
+    }
+
+    e.preventDefault();
+}
