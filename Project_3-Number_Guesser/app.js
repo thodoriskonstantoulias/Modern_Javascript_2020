e03dc92ba2ab8b45846c8edc5b60b7788acb6bad
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-01-15 00:30:31 +0200

    Project : Number Guesser Part 2 : Validation and winning case implementation

--- Project_3-Number_Guesser/app.js
+++ Project_3-Number_Guesser/app.js
@@ -1,1 +1,36 @@
-    
+let min = 1,
+    max = 10,
+    winningNum = 2,
+    guessesLeft = 3;
+
+const game = document.querySelector('#game');
+const minNum = document.querySelector('.min-num');   
+const maxNum = document.querySelector('.max-num'); 
+const guessBtn = document.querySelector('#guess-btn');
+const guessInput = document.querySelector('#guess-input');
+const message = document.querySelector('.message');
+
+minNum.textContent = min;
+maxNum.textContent = max;
+
+guessBtn.addEventListener('click', function(){
+    let guess = parseInt(guessInput.value);
+
+    //Validation
+    if (isNaN(guess) || guess < min || guess > max){
+        setMessage(`Please enter a number between ${min} and ${max}`, 'red');
+    }
+
+    if (guess === winningNum){
+        guessInput.disabled = true;
+        guessInput.style.borderColor = 'green';
+        setMessage(`${winningNum} is correct`, 'green');
+    } else {
+         
+    }
+});
+
+function setMessage(msg, color){
+    message.style.color = color;
+    message.textContent = msg;
+}
