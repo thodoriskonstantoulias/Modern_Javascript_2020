e783a870dc2d86e26bf4cb4a3b66539fa62b75e5
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-01-15 21:47:33 +0200

    OOP : Constructors, this keyword, Build in constructors

---
+++ OOP/app.js
@@ -0,0 +1,20 @@
+// 1 -- Constructors and this keyword
+// function Person(name,age){
+//     this.name = name;
+//     this.age = age;
+
+// }
+
+// let ted = new Person('Ted', 28);
+// console.log(ted);
+
+// 2 -- Build in constructors
+
+//The following is a string
+//const name1 = 'Ted';
+//The following is an object
+// const name2 = new String('Ted');
+
+// console.log(name2);
+// console.log(typeof name2);
+//The same happens with numbers, boolean, functions, objects, arrays, regex
