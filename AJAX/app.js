3aa7ffa58cbdd8ac59f85402a61ae36d3860e8cb
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-01-16 21:57:14 +0200

    AJAX : Working with JSON data

--- AJAX/app.js
+++ AJAX/app.js
@@ -28,3 +28,63 @@
 
 //     xhr.send();
 // }
+
+// 2 -- JSON 
+// document.getElementById('button1').addEventListener('click', loadCustomer);
+// document.getElementById('button2').addEventListener('click', loadCustomers);
+
+// // JSON with one object
+// function loadCustomer(e){
+//      //Create an XHR object
+//      const xhr = new XMLHttpRequest();
+//      xhr.open('GET','customer.json',true);
+
+//         xhr.onload = function(){
+//         if (this.status === 200){
+//             //console.log(this.responseText);
+//             const customer = JSON.parse(this.responseText);
+
+//             const output = `
+//                 <ul>
+//                     <li>ID : ${customer.id}</li>
+//                     <li>NAME : ${customer.name}</li>
+//                     <li>COMPANY : ${customer.company}</li>
+//                     <li>PHONE : ${customer.phone}</li>
+//                 </ul>
+//             `;
+
+//             document.getElementById('customer').innerHTML = output;
+//          }
+//         }
+
+//      xhr.send();
+// }
+// //JSON with array of objects
+// function loadCustomers(e){
+//     //Create an XHR object
+//     const xhr = new XMLHttpRequest();
+//     xhr.open('GET','customers.json',true);
+
+//        xhr.onload = function(){
+//        if (this.status === 200){
+//            //console.log(this.responseText);
+//            const customers = JSON.parse(this.responseText);
+
+//            let output = '';
+//             customers.forEach(function(customer){
+//                 output += `
+//                 <ul>
+//                     <li>ID : ${customer.id}</li>
+//                     <li>NAME : ${customer.name}</li>
+//                     <li>COMPANY : ${customer.company}</li>
+//                     <li>PHONE : ${customer.phone}</li>
+//                 </ul>
+//             `;
+//             });
+
+//            document.getElementById('customers').innerHTML = output;
+//          }
+//        }
+
+//     xhr.send();
+// }
