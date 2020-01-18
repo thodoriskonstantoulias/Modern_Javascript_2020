f8f6d9fbe67321b0a93a947564a8923dd76e79a6
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-01-18 16:13:26 +0200

    Project : Github Finder Part 5 : Fetch and display repos of user

--- Project_5-Github_Finder/github.js
+++ Project_5-Github_Finder/github.js
@@ -1,11 +1,23 @@
 class Github {
 
+    constructor(){
+        this.repos_count = 5;
+        this.repos_sort = 'created:asc';
+    }
+
     async getUser(user){
+        //Fetch the users
         const profileResponse = await fetch(`https://api.github.com/users/${user}`);
+        //Fetch their repos
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}`);
+
         const profileData = await profileResponse.json();
+        const repos = await repoResponse.json();
+
 
         return {
-            profile : profileData
+            profile : profileData,
+            repos : repos
         }
     }
 }
