de5d138a9a1640428fbf487738e45bed71cf1457
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-01-19 12:40:41 +0200

    REGEX : Metacharacters, Character sets, Shorthand character classes, Assertions

--- REGEX/app.js
+++ REGEX/app.js
@@ -1,7 +1,7 @@
 //1 -- REGEX intro
 
-let re;
-re = /hello/i;  // i = case insensitive
+//let re;
+//re = /hello/i;  // i = case insensitive
 //console.log(re.source);
 
 //exec -- return result in an array or null
@@ -21,3 +21,55 @@ re = /hello/i;  // i = case insensitive
 // const str = 'hello man';
 // const result = str.replace(re,'hi');
 // console.log(result);
+
+//2 -- Metacharacters
+let re;
+//Literal chars
+re =  /hello/;
+re =  /hello/i;
+
+//Metacharacters 
+re = /^h/i; // Must start with
+re = /d$/i; // Must end with
+re = /h.llo/i // Matches any ONE character
+re = /h*llo/i // Matches any character 0 or more times
+
+//Character sets
+re = /gr[ae]y/i;  //Must be an a or an e 
+re = /[GF]ray/; 
+re = /[^GF]ray/i; //Match anything except G or F 
+re = /[A-Z]ray/; //Match any uppercase letter 
+re = /[A-Za-z]ray/; //Match any letter 
+re = /[0-9]ray/; //Match any number 
+
+//Quantifiers 
+re = /Hel{2}o/i // l must occurs two times
+
+//Parentheses - Grouping
+re = /([0-9]x){3}/
+
+//Shorthand char classes
+re = /\w/; //Word character - alphanumeric or _
+re = /\w+/; //+ = one or more
+re = /\d/; // match any digit
+
+//Assertion
+re = /x(?=y)/; // Match x only if followed by y
+
+//const str = 'Hello world';
+//const str = 'Gray';
+//const str = '5x4x4x';
+const str = 'xy';
+
+const result = re.exec(str);
+console.log(result);
+
+function reTest(re,str){
+    if (re.test(str)){
+        console.log(`${str} matches ${re.source}`);
+    }else {
+        console.log(`${str} does not matches ${re.source}`)
+    }
+}
+
+reTest(re,str);
