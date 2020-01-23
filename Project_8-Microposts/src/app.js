23ab4edae1ef83b1b7bd223f1942b5e10943ee4e
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-01-23 21:41:24 +0200

    Project : Microposts Part 6 : DELETE posts and alerts

--- Project_8-Microposts/src/app.js
+++ Project_8-Microposts/src/app.js
@@ -3,6 +3,7 @@ import {ui} from './ui';
 
 document.addEventListener('DOMContentLoaded', getPosts);
 document.querySelector('.post-submit').addEventListener('click', submitPost);
+document.querySelector('#posts').addEventListener('click', deletePost);
 
 function getPosts(){
     http.get('http://localhost:3000/posts')
@@ -26,4 +27,20 @@ function submitPost(){
             getPosts();
         })
         .catch(err => console.log(err));
+}
+
+function deletePost(e){
+    if (e.target.parentElement.classList.contains('delete')){
+        const id = e.target.parentElement.dataset.id;
+        if (confirm('Are you sure?')){
+            http.delete(`http://localhost:3000/posts/${id}`)
+                .then(data => {
+                    ui.showAlert('Post Deleted', 'alert alert-success');
+                    getPosts();
+                })
+                .catch(err => console.log(err));
+        }
+    }
+
+    e.preventDefault();
 }
